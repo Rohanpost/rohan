934ba86ff519d8ee0e1bd58d3a5d5d2c069ae717
Hotkar <rhn.htkr@gmail.com>
2023-02-28 19:32:50 +0530

    modified

---
+++ src/BroserShow/Demosel.java
@@ -0,0 +1,15 @@
+package BroserShow;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demosel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	void launchbrowser() {
+		ChromeDriver driver =new chromeDriver();
+	}
+}
